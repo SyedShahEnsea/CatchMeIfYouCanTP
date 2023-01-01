51aa275e5bec382336dd1634017c3aa56cb5d817
Syed Shah <dovah@Syeds-MacBook-Pro.local>
2023-01-01 04:53:59 +0100

    Ajout Rotation

--- src/Earth.java
+++ src/Earth.java
@@ -1,3 +1,5 @@
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.paint.PhongMaterial;
@@ -20,6 +22,18 @@ public class Earth extends Group{
         this.material.setDiffuseMap(earth);
         this.sph.setMaterial(this.material);
         this.getChildren().add(this.sph);
+        this.getTransforms().add(this.ry);
+        this.ry.setAxis(Rotate.Y_AXIS);
+
+        AnimationTimer animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long time) {
+                System.out.println("Valeur de time : " + time);
+                ry.setAngle(time/(1.5e10/360));
+
+            }
+        };
+        animationTimer.start();
     }
     public void createSphere(){
         this.sph = new Sphere(300);
